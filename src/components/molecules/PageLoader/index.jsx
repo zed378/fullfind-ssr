80f854a6168fc6af80799fa5370879fb6de49747
Zed <kembedt@gmail.com>
2024-12-25 22:35:44 +0700

    deelte some component for debugging

--- src/components/molecules/PageLoader/index.jsx
+++
@@ -1,9 +0,0 @@
-import { HashLoader } from "react-spinners";
-
-export default function PageLoader() {
-  return (
-    <div className="absolute top-0 left-0 z-50 w-full h-full opacity-80 bg-slate-300 flex flex-col items-center justify-center">
-      <HashLoader color="#468499" size={150} />
-    </div>
-  );
-}
